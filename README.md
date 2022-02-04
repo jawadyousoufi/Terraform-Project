97f3e1a91c5bd37a3c9923dbdbfb3ce9043ee19e
tunaytas <tnc.tas@gmail.com>
2022-02-04 16:28:55 +0000

    RDSupdate

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Terraform-Project Team 1 RDS
+# Create 3 reader 1 writer cluster of 4 number of instances. Auto scaling enabled. 
+## Resources created:
+```
+ module "rds cluster" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = "Team1-RDS"
+  engine         = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2-micro"
+  instances = {
+    one = {}
+    3 = {
+      instance_class = "t2-micro"
+    }
+  }
+    
+  ```
--- rds.tf
+++ rds.tf
@@ -1,33 +1,32 @@
-resource "aws_rds_cluster" "default" {
-	cluster_identifier = var.identifier
-	engine = var.engine
-	engine_version = var.engine_version
-	database_name = var.identifier
-	master_username = var.username
-	master_password = random_password.password.result
-	db_subnet_group_name = aws_db_subnet_group.db.name
-	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported
-	vpc_security_group_ids = [
-		aws_security_group.db.id
-	]
-}
-resource "aws_rds_cluster_instance" "cluster_instances-writer" {
-	count = 1
-	identifier = "aurora-cluster-demo-${count.index +1}"
-	cluster_identifier = var.identifier
-	instance_class = "db.t2.small"
-	engine_version = var.engine_version
-	engine = var.engine
-	publicly_accessible = var.publicly_accessible
-	
-}
-resource "aws_rds_cluster_instance" "cluster_instances-reader" {
-	count = 3
-	identifier = "aurora-cluster-demo-reader-${count.index +2}"
-	cluster_identifier = var.identifier
-	instance_class = "db.t2.small"
-	engine_version = var.engine_version
-	engine = var.engine
-	publicly_accessible = var.publicly_accessible
-	
+module "rds cluster" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = "Team1-RDS"
+  engine         = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t2-micro"
+  instances = {
+    one = {}
+    3 = {
+      instance_class = "db.t2-micro"
+    }
+  }
+
+  vpc_id  = "default-vpc"
+  subnets = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  allowed_security_groups = ["sg-vpc"]
+  allowed_cidr_blocks     = ["10.20.0.0/20"]
+
+  storage_encrypted   = true
+  apply_immediately   = true
+
+  db_parameter_group_name         = "default"
+  db_cluster_parameter_group_name = "default"
+
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
