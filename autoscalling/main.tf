43cda3542a03d9acdf7609e380e84443659a3e2f
tunaytas <tnc.tas@gmail.com>
2022-02-06 02:59:37 +0000

    update 9 pm

--- autoscalling/main.tf
+++ autoscalling/main.tf
@@ -35,10 +35,10 @@ resource "aws_launch_template" "webserver" {
     instance_type = "t2.micro"
     user_data = file("/home/ec2-user/Terraformaws/autoscalling/wordpress.sh")
     key_name = var.ssh_keypair
-    iam_instance_profile {
-      name = module.iam_instance_profile.name
-    }
-    vpc_security_group_ids = [var.sg.webserver]
+    # iam_instance_profile {
+    #   name = module.iam_instance_profile.name
+    # }
+    vpc_security_group_ids = [var.sg.websvr]
 }
 resource "aws_autoscaling_group" "webserver" {
   name                      = "${var.namespace}-asg"
@@ -47,19 +47,19 @@ resource "aws_autoscaling_group" "webserver" {
   vpc_zone_identifier       = var.vpc.private_subnets
   target_group_arns         =  module.alb.target_group_arns
   launch_template {
-      id = aws_launch_template.webserver.vpc_id
-      version = aws_launch_template.webserver.latest.version
+      id = aws_launch_template.webserver.id
+      version = aws_launch_template.webserver.latest_version
   }
 }
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 5.0"
 
-  name = "var.namespace"
+  name = var.namespace
 
   load_balancer_type = "application"
 
-  vpc_id             = var.vpc_vpc_id
+  vpc_id             = var.vpc.vpc_id
   subnets            = var.vpc.public_subnets
   security_groups    = [var.sg.lb]
   http_tcp_listeners = [
--- networking/main.tf
+++ networking/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   version = "2.64.0"
   name = "my-vpc"
   cidr = "10.0.0.0/16"
-  azs             =  data.aws_availability_zones.available.names #ask farukh
+  azs             =  data.aws_availability_zones.available.names 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
